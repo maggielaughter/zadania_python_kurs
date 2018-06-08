778484138bec1b3f393b4e58ac817eb403715f06
maggielaughter <smiechmalgosia@gmail.com>
2018-06-08 22:48:18 +0200

    fixed code - pesel moduls

--- pesel_moduls.py
+++ pesel_moduls.py
@@ -3,24 +3,40 @@ nineteenth_century = ['81', '82', '83', '84', '85', '86', '87', '88', '89', '90'
 twentieth_century = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
 twentyfirst_century = ['21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32']
 # months indexes
-months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']
+
+
+def getPeselYear(pesel):
+    pesel_month = pesel[2:4]
+    pesel_year = pesel[0:2]
+    if pesel_month in nineteenth_century:
+        pesel_full_year = '18' + pesel_year
+    elif pesel_month in twentieth_century:
+        pesel_full_year = '19' + pesel_year
+    else:
+        pesel_full_year = '20' + pesel_year
+    return pesel_full_year
 
 def PeselValidation(pesel):
-    p=[]
+    """p=[]
 
-    for char in pesel:
-        p.append(int(char))
+        for char in pesel:
+        p.append(int(char))"""
+    p=[int(pi) for pi in pesel]
+    print(p)
 
     sum_pesel=p[0]*9+p[1]*7+p[2]*3+p[3]*1+p[4]*9+p[5]*7+p[6]*3+p[7]*1+p[8]*9+p[9]*7
     sum_con=sum_pesel%10
 
-    def DaysCheck(year, month, day):
+    def DaysCheck(pesel):
 
-        year=getPeselYear(pesel)
+        year=int(getPeselYear(pesel))
         month=pesel[2:4]
-        day=pesel[4:6]
+        day=int(pesel[4:6])
 
-        if (yesr % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        print(month)
+
+        if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
             days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
         else:
             days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
@@ -31,12 +47,12 @@ def PeselValidation(pesel):
         if month not in months:
             raise ValueError('Niepoprawny miesiac')
 
-        if day < 0 or day > days[month - 1]:
+        if day < 0 or day > days[int(month) - 1]:
             raise ValueError('Niepoprawna ilosc dni')
 
         return True
 
-    return sum_con==int(pesel[-1]) and len(pesel)==11
+    return sum_con==int(pesel[-1]) and len(pesel)==11 and DaysCheck(pesel)
 
 def PeselInfo(pesel):
     if PeselValidation(pesel) is False:
@@ -52,26 +68,8 @@ def PeselInfo(pesel):
     addKeyValue(dictionary, twentyfirst_century)
 
 
-    def getPeselYear(pesel):
-        pesel_month=pesel[2:4]
-        pesel_year=pesel[0:2]
-        if pesel_month in nineteenth_century:
-            pesel_full_year='18'+pesel_year
-        elif pesel_month in twentieth_century:
-            pesel_full_year='19'+pesel_year
-        else:
-            pesel_full_year='20'+pesel_year
-        return pesel_full_year
-
-
-    def formatMonth(month):
-        if len(month)==1:
-            full_month='0'+month
-        return full_month
-
-
     year=getPeselYear(pesel)
-    month=formatMonth(dictionary[pesel[2:4]])
+    month=pesel[2:4]
     day=pesel[4:6]
     full_birth_date=year+'-'+month+'-'+day
 
